63d37003e729dfa19e6afe60dab67e230e110720
Gerard Tieng <gerardtieng@Gerards-MacBook-Pro.local>
2019-12-24 12:51:43 -0800

    filter and display complete

--- static/js/app.js
+++ static/js/app.js
@@ -3,9 +3,9 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-// inspect the HTML and select the button element with d3
+// inspect the HTML and select elements for interaction (button and table)
 var button = d3.select("#filter-btn");
-
+var body = d3.select("tbody");
 
 // code for when button is clicked
 button.on("click", function() {
@@ -18,4 +18,16 @@ button.on("click", function() {
 
     //filter through the data for matches to entered datetime value
     var filteredData = tableData.filter(i => i.datetime === value);
+    
+    //loop through object
+    filteredData.forEach( (entry) => {
+        //add a new row for each object in the dataset
+        var row = body.append("tr");
+
+        //within each row, add a cell and fill it with the value
+        Object.entries(entry).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 });
